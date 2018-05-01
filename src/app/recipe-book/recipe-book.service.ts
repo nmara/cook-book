37c6a1effa9f1c8d1b4e3c73c6ce3e486245cd11
nmara <aga.czekala89@gmail.com>
2018-05-01 17:51:54 +0200

    adding ingredients

--- src/app/recipe-book/recipe-book.service.ts
+++ src/app/recipe-book/recipe-book.service.ts
@@ -1,12 +1,35 @@
 import { EventEmitter } from '@angular/core';
 import { RecipeBook } from './recipe-book.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeBookService {
   recipeSelected = new EventEmitter<RecipeBook>();
 
   private recipes: RecipeBook[] = [
-    new RecipeBook ('Test name', 'Test desc', 'http://img.bestrecipes.com.au/rZFo7F8i/h300-w400-cscale-1495077669/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
-    new RecipeBook ('Test name2', 'Test desc2', 'http://img.bestrecipes.com.au/rZFo7F8i/h300-w400-cscale-1495077669/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg')
+    new RecipeBook (
+    'Apple Cake',
+    'The best cake in the entire world!', 'https://2.bp.blogspot.com/-m2y8US9jJJY/V_dJKApCJ1I/AAAAAAAAWtc/Kl9E5zr0zfQVM4Y7M80pCH5JnR7LkL2ywCLcB/s1600/szarlotka3.jpg',
+    [
+      new Ingredient('apples', 10),
+      new Ingredient('flour', 700),
+      new Ingredient('butter', 1),
+      new Ingredient('sugar', 200),
+      new Ingredient('eggs', 3)
+    ]),
+    new RecipeBook (
+    'Lasagne',
+    'Yep! Easy and delicous!', 'https://cdn.katalogsmakow.pl/2017/02/02/0x600/lasagne-bolognese.558680.jpg',
+    [
+      new Ingredient('meat', 600),
+      new Ingredient('tomatoes', 1),
+      new Ingredient('onion', 1),
+      new Ingredient('butter', 200),
+      new Ingredient('milk', 200),
+      new Ingredient('flour', 20),
+      new Ingredient('garlic', 2),
+      new Ingredient('lasagne pasta', 1),
+      new Ingredient('mozzarella', 250)
+    ])
   ];
 
   getRecipes() {
