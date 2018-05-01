268c754a2b7c88d5f2ac40567759894979dffe75
nmara <aga.czekala89@gmail.com>
2018-05-01 18:29:11 +0200

    ingredients to shopping list

--- src/app/recipe-book/recipe-book.service.ts
+++ src/app/recipe-book/recipe-book.service.ts
@@ -1,7 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { RecipeBook } from './recipe-book.model';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeBookService {
   recipeSelected = new EventEmitter<RecipeBook>();
 
@@ -32,7 +34,13 @@ export class RecipeBookService {
     ])
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- src/app/recipe-book/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { RecipeBook } from '../recipe-book.model';
+import { RecipeBookService } from '../recipe-book.service';
 
 @Component({
-    selector: 'app-recipe-detail',
-    templateUrl: './recipe-detail.component.html',
-    styleUrls: ['./recipe-detail.component.css']
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-    @Input() recipe: RecipeBook;
+  @Input() recipe: RecipeBook;
 
-    constructor() { }
+  constructor(private recipeBookService: RecipeBookService) { }
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
+
+  onAddToShoppingList() {
+    this.recipeBookService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 
 }
